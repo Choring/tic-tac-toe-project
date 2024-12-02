d2de0a22336a09142bf9ca62c5d8a6f6f7259af6
Choring <skyhonor201@gmail.com>
2024-12-02 13:11:23 +0900

    State 끌어올리기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import { useState } from "react";
 import {GameBoard} from "./components/GameBoard";
 import {Player} from "./components/Player";
+import { Log } from "./components/Log";
+
+interface Turn {
+  square: { row: number; col: number };
+  player: "X" | "O";
+}
 
 function App() {
+  const [gameTurn, setGameTurn] = useState<Turn[]>([]);
   const [activePlayer, setActivePlayer] = useState<"X" | "O">("X");
 
-  const handleSelectSquare = () => {
+  const handleSelectSquare = (rowIndex : number, colIndex: number): void => {
     setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X" );
+    setGameTurn(prevTurns => {
+      let currentPlayer = "X";
+
+      if(prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      } 
+      const updatedTurns = [
+        { square: {row: rowIndex, col: colIndex} , 
+        player: currentPlayer} , 
+        ...prevTurns
+      ];
+      return updatedTurns;
+    });
+
+    
   };
 
   return <main>
@@ -15,8 +37,9 @@ function App() {
         <Player initialName="Player 1" symbol="X" isActive= {activePlayer === "X"} />
         <Player initialName="Player 2" symbol="O" isActive= {activePlayer === "O"} />
       </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol= {activePlayer} />
+      <GameBoard onSelectSquare={handleSelectSquare} turns = {gameTurn} />
     </div>
+    <Log />
   </main>;
 }
 
--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,37 +1,49 @@
-import { useState } from "react";
 
-type GameBoard = (null | string)[][];
+
+type GameBoard = (null | "X" | "O")[][];
+interface Turn {
+    square: { row: number; col: number };
+    player: "X" | "O";
+}
 interface GameBoardProps {
-    activePlayerSymbol: "X" | "O";
+    turns: Turn[];
     onSelectSquare: (rowIndex: number, colIndex: number) => void;
 }
 
-
-const initialGameBoard: null[][] = [
+const initialGameBoard: GameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export const GameBoard: React.FC<GameBoardProps> = ({ onSelectSquare, activePlayerSymbol }) => {
-    const [gameBoard, setGameBoard ] = useState<GameBoard>(initialGameBoard);
+export const GameBoard: React.FC<GameBoardProps> = ({ onSelectSquare, turns }) => {
+    const gameBoard = initialGameBoard;
 
-    const handleSelectSquare :(rowIndex: number, colIndex: number) => void  = (rowIndex, colIndex) => {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard: GameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
+    for (const turn of turns ) {
+        const { square, player } = turn;
+        const { row, col } = square;
+        
+        gameBoard[row][col] = player;
+    }
 
-        onSelectSquare(rowIndex, colIndex);
-    };
+    // const [gameBoard, setGameBoard ] = useState<GameBoard>(initialGameBoard);
+
+    // const handleSelectSquare :(rowIndex: number, colIndex: number) => void  = (rowIndex, colIndex) => {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard: GameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+
+    //     onSelectSquare(rowIndex, colIndex);
+    // };
 
     return <ol id="game-board">
         {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex) => 
                 <li key={colIndex}>
-                    <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                    <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                 </li>)}
             </ol>
         </li>)}
