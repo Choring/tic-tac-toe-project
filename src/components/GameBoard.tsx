c793eaf6e913eccfe77fb83a282cc0f94fc930d5
Choring <skyhonor201@gmail.com>
2024-12-02 12:36:54 +0900

    GameBoard에서 X or O 추가

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+
+type GameBoard = (null | string)[][];
+interface GameBoardProps {
+    activePlayerSymbol: "X" | "O";
+    onSelectSquare: (rowIndex: number, colIndex: number) => void;
+}
 
 
-const initialGameBoard = [
+const initialGameBoard: null[][] = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export default function GameBoard() {
+export const GameBoard: React.FC<GameBoardProps> = ({ onSelectSquare, activePlayerSymbol }) => {
+    const [gameBoard, setGameBoard ] = useState<GameBoard>(initialGameBoard);
+
+    const handleSelectSquare :(rowIndex: number, colIndex: number) => void  = (rowIndex, colIndex) => {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard: GameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedBoard;
+        });
+
+        onSelectSquare(rowIndex, colIndex);
+    };
+
     return <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex) => 
                 <li key={colIndex}>
-                    <button>{playerSymbol}</button>
+                    <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                 </li>)}
             </ol>
         </li>)}
