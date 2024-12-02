502a603f7e841360d048d961cf78a4bef63c1875
Choring <skyhonor201@gmail.com>
2024-12-02 17:37:50 +0900

    에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -12,30 +12,47 @@ function App() {
   const [gameTurn, setGameTurn] = useState<Turn[]>([]);
   const [activePlayer, setActivePlayer] = useState<"X" | "O">("X");
 
-  const handleSelectSquare = (rowIndex : number, colIndex: number): void => {
-    setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X" );
-    setGameTurn(prevTurns => {
-      let currentPlayer = "X";
+  // const handleSelectSquare = (rowIndex : number, colIndex: number): void => {
+  //   setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X" );
+  //   setGameTurn(prevTurns => {
+  //     let currentPlayer = "X";
 
-      if(prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      } 
-      const updatedTurns = [
-        { square: {row: rowIndex, col: colIndex} , 
-        player: currentPlayer} , 
-        ...prevTurns
-      ];
-      return updatedTurns;
-    });
+  //     if(prevTurns.length > 0 && prevTurns[0].player === "X") {
+  //       currentPlayer = "O";
+  //     } 
+  //     const updatedTurns = [
+  //       { square: {row: rowIndex, col: colIndex} , 
+  //       player: currentPlayer} , 
+  //       ...prevTurns
+  //     ];
+  //     return updatedTurns;
+  //   });
 
     
+  // };
+
+  const handleSelectSquare = (rowIndex: number, colIndex: number): void => {
+    setGameTurn((prevTurns) => [
+      { square: { row: rowIndex, col: colIndex }, player: activePlayer },
+      ...prevTurns,
+    ]);
+
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
   };
 
   return <main>
     <div id="game-container">
       <ol id="players" className="highlight-player">
-        <Player initialName="Player 1" symbol="X" isActive= {activePlayer === "X"} />
-        <Player initialName="Player 2" symbol="O" isActive= {activePlayer === "O"} />
+        <Player 
+          initialName="Player 1" 
+          symbol="X" 
+          isActive= {activePlayer === "X"} 
+        />
+        <Player 
+          initialName="Player 2" 
+          symbol="O" 
+          isActive= {activePlayer === "O"} 
+        />
       </ol>
       <GameBoard onSelectSquare={handleSelectSquare} turns = {gameTurn} />
     </div>
